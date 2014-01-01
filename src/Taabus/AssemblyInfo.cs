c914942bb38d27847e38f8c1bfe3f3c2d4c2ad07
hahoyer <hahoyer@gmail.com>
2014-01-01 03:49:22 +0100

    Admin: AssemblyInfo

--- src/Taabus/AssemblyInfo.cs
+++ src/Taabus/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 [assembly: AssemblyDescription("Database table browser")]
 [assembly: AssemblyCompany("HoyerWare")]
 [assembly: AssemblyProduct("Taabus")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyVersion("13.0.0.354")]
+[assembly: AssemblyCopyright("Copyright © Harald Hoyer 2013 - 2014")]
+[assembly: AssemblyVersion("14.0.0.0")]

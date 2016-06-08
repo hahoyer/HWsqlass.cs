cf06b1a0d8fa235f7fc2c9f06ccce62ed0013121
hahoyer <hahoyer@gmail.com>
2016-06-08 09:05:44 +0200

    New: Logging

--- src/Clock2/Clock.cs
+++ src/Clock2/Clock.cs
@@ -1,40 +1,26 @@
-﻿#region Copyright (C) 2013
-
-//     Project Clock2
-//     Copyright (C) 2013 - 2013 Harald Hoyer
-// 
-//     This program is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-// 
-//     This program is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-// 
-//     You should have received a copy of the GNU General Public License
-//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//     
-//     Comments, bugs and suggestions to hahoyer at yahoo.de
-
-#endregion
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
+using hw.Helper;
+using log4net;
+using log4net.Config;
 
 namespace Clock2
 {
     sealed partial class Clock : Form
     {
+        static readonly ILog Logger = LogManager.GetLogger(typeof(Clock));
+
         public Clock()
         {
+            XmlConfigurator.Configure(new FileInfo("log4net.xml"));
             InitializeComponent();
             InitializeChildren(_date, _time);
         }
+
         void InitializeChildren(params Control[] controls)
         {
             foreach(var label in controls)
@@ -47,6 +33,47 @@ namespace Clock2
 
         void TimerTick(object sender, EventArgs e)
         {
+            UpdateUserInterface();
+            LogBounds();
+            LogScreenLayout();
+        }
+
+        void LogScreenLayout()
+        {
+            var text = GetScreenLayout();
+            if(LastScreenLayoutText == text)
+                return;
+
+            LastScreenLayoutText = text;
+            Logger.Info(text);
+        }
+
+        void LogBounds()
+        {
+            var text = Format(Bounds);
+            if (LastBoundsText == text)
+                return;
+
+            LastBoundsText = text;
+            Logger.Info(text);
+        }
+
+        static string GetScreenLayout()
+            => Screen
+                .AllScreens
+                .Select(Format)
+                .Stringify("; ");
+
+        static string Format(Screen value)
+            => (value.Primary ? "*" : "") + value.DeviceName + "(" + Format(value.Bounds) + ")";
+
+        static string Format(Rectangle value)
+            => Format(value.Left, value.Right) + "/" + Format(value.Top, value.Bottom);
+
+        static string Format(int low, int high) => low + "[" + (high - low) + "]" + high;
+
+        void UpdateUserInterface()
+        {
             var dateTime = DateTime.Now;
             var date = dateTime.ToString("ddd dd.MM");
             _date.Text = date;
@@ -57,9 +84,15 @@ namespace Clock2
         }
 
         Size? _clickedPoint;
+        string LastScreenLayoutText;
+        string LastBoundsText;
 
         void HandleMouseUp(object sender, MouseEventArgs e) { _clickedPoint = null; }
-        void HandleMouseDown(object sender, MouseEventArgs e) { _clickedPoint = new Size(e.X, e.Y); }
+
+        void HandleMouseDown(object sender, MouseEventArgs e)
+        {
+            _clickedPoint = new Size(e.X, e.Y);
+        }
 
         void HandleMouseMove(object sender, MouseEventArgs e)
         {
@@ -67,9 +100,10 @@ namespace Clock2
                 Location = PointToScreen(e.Location) - _clickedPoint.Value;
         }
 
-        void MenuAnchorMouseClick(object sender, MouseEventArgs e) { _mainMenu.Show(PointToScreen(e.Location + new Size(_menuAnchor.Location))); }
+        void MenuAnchorMouseClick(object sender, MouseEventArgs e)
+            => _mainMenu.Show(PointToScreen(e.Location + new Size(_menuAnchor.Location)));
 
-        void CloseClick(object sender, EventArgs e) { Close(); }
-        protected override string ConfigFileName { get { return "Clock"; } }
+        void CloseClick(object sender, EventArgs e) => Close();
+        protected override string ConfigFileName => "Clock";
     }
 }
